3e270f19bd33c1f469d41797b7830c8c85c3b322
IEROGLIF <ieroglif.kerch@gmail.com>
2026-04-02 13:11:18 +0300

    Поправил баги

--- js/img-scale.js
+++ js/img-scale.js
@@ -3,15 +3,15 @@ import { uploadForm } from './upload-form.js';
 const SCALE = {
   STEP: 25,
   MAX: 100,
-  MIN: 0,
-  DEFAULT: '100%',
+  MIN: 25,
+  DEFAULT: 100,
 };
 const scaleMinus = uploadForm.querySelector('.scale__control--smaller');
 const scalePlus = uploadForm.querySelector('.scale__control--bigger');
 const scaleValue = uploadForm.querySelector('.scale__control--value');
 const uploadImg = uploadForm.querySelector('.img-upload__preview img');
 
-const isScaleInRange = (newScale) => newScale <= SCALE.MAX && newScale > SCALE.MIN;
+const isScaleInRange = (newScale) => newScale <= SCALE.MAX && newScale >= SCALE.MIN;
 
 const applyScale = (isPlus) => {
   let scale = parseInt(scaleValue.value, 10);
@@ -21,13 +21,14 @@ const applyScale = (isPlus) => {
     scale = scale - SCALE.STEP ;
   }
   if (isScaleInRange(scale)) {
-    scaleValue.value = `${scale} %`;
+    scaleValue.value = `${scale}%`;
     uploadImg.style.transform = `scale(${scale / 100})`;
   }
 };
 
 const scaleReset = () => {
   scaleValue.value = SCALE.DEFAULT;
+  uploadImg.style.transform = `scale(${SCALE.DEFAULT / 100})`;
 };
 
 const onClickScaleMinus = () => applyScale(false);
