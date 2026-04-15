0028fe53ec322267457353be81233d5a8eb83cc7
IEROGLIF <ieroglif.kerch@gmail.com>
2026-04-15 11:29:47 +0300

    оптимизировал функцию случайных фоток.

--- js/util.js
+++ js/util.js
@@ -1,6 +1,7 @@
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const isEsc = (evt) => evt.key === 'Escape';
-function debounce (callback, timeoutDelay = 500) {
+
+function debounce(callback, timeoutDelay = 500) {
   // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
   // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
   let timeoutId;
@@ -18,4 +19,12 @@ function debounce (callback, timeoutDelay = 500) {
   };
 }
 
-export {getRandomInt, isEsc, debounce};
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+export { getRandomInt, isEsc, debounce, shuffleArray };
